3d83ce5aa342104276ec269f3ade79b82a15baed
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-05-25 14:19:33 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,17 +1,17 @@
-HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+HELP_TEXT = """__**Aku bisa memutar lagu di voice chat**__
 
-**/help** __Show This Message.__
-**/skip** __Skip The Current Playing Music.__
-**/play** __youtube/saavn/deezer Song_Name__
-**/telegram** __Play From Telegram Audio.__
-**/joinvc** __Join Voice Chat.__
-**/leavevc** __Leave Voice Chat.__
-**/volume [1-200]** __Adjust Volume.__
-**/pause** __Pause Music.__
-**/resume** __Resume Music.__
+**/help** __Pesan ini.__
+**/skip** Tau lah
+**/play** youtube/saavn/deezer Nama lagu
+**/telegram** __Mulai dari telegram audio.__
+**/joinvc** __Masuk Voice Chat.__
+**/leavevc** __Keluar Voice Chat.__
+**/volume [1-200]** __Membesarkan volume.__
+**/pause** __Pause musik.__
+**/resume** __Resume Musik.__
 **/update** __Update & Restart.__"""
 
-START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. Join @TGVCSUPPORT For Support.**__"
+START_TEXT = "__**Hai aku bot telegram voice chat, untuk memutar lagu di voice chat mu 🎧**__"
 
 REPO_TEXT = (
     "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
